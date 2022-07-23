f81db0e48e1c7130b4f7102edacd8b7c791b9965
Edilson-EJ <edilsonjunior624@gmail.com>
2022-07-23 10:01:11 -0300

    projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import './style.css';
 
 export function App() { 
 
   return (
     <div>
+      <Header />
+
       <Post 
         author="Edilson" 
         content="Hello React"
