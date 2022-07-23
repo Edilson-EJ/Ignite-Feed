41597087d1de83478ff3c5060dfcf24d68658fec
Edilson-EJ <edilsonjunior624@gmail.com>
2022-07-22 21:12:34 -0300

    Iniciando React

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Post } from "./Post";
+
+export function App() { 
+
+  return (
+    <div>
+      <Post 
+        author="Edilson" 
+        content="Hello React"
+      />
+      
+    </div>
+    
+  )
+}
+
+
